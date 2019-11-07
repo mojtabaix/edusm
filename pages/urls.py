7e9b27877b1df90080e479048d715d36adb343d1
sahel <mesforoshs@gmail.com>
2019-11-07 18:16:39 +0330

    second

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from pages.views import homepage, signuppage, loginpage
+from pages.views import homepage, signuppage, loginpage, contactPage
 
 urlpatterns = [
     path('', homepage, name='home'),
     path('register/', signuppage, name='register'),
     path('login/', loginpage, name='login'),
+    path('contact_us/', contactPage, name='contact_us')
 ]
