55aab834c9ecae4807b3dbfbe2bb2afca103b952
sahel <mesforoshs@gmail.com>
2019-11-07 16:36:31 +0330

    first commit

--- pages/forms.py
+++ pages/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
+from django.utils.translation import gettext, gettext_lazy as _
 from pages.models import Student
 
 
@@ -9,12 +9,22 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=50, help_text='required', )
     email = forms.EmailField(max_length=300, help_text='activation code will be sent to this email.enter a valid email')
     first_name = forms.CharField(max_length=50, help_text='required')
+    error_messages = {
+        'password_mismatch': _("گذرواژه و تکرار گذرواژه یکسان نیستند"),
+    }
 
     class Meta:
         model = User
         fields = ('username', 'last_name', 'first_name', 'email', 'password1', 'password2')
 
-    def is_valid(self):
-        if not super().is_valid():
-            return False
-        data = self.cleaned_data
+    # def is_valid(self):
+    #     if User.objects.filter(username=self.cleaned_data['username']).exists():
+    #         raise forms.ValidationError(_("نام کاربری شما در سیستم موجود است"))
+    #     if not super().is_valid():
+    #         return False
+
+
+
+
+class LoginForm(forms.Form):
+    user_name = forms.CharField(max_length=50)
--- pages/views.py
+++ pages/views.py
@@ -1,31 +1,40 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from pages.forms import SignUpForm
+from pages.forms import SignUpForm, LoginForm
 
 
 def homepage(request):
-    return render(request, 'home.html')
+    return render(request, 'base1.html')
 def signuppage(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()
-            user.student2.last_name = form.cleaned_data.get('last_name')
+            user.student.last_name = form.cleaned_data['last_name']
             user.save()
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=password)
+            username = form.cleaned_data['username']
+            raw_password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return HttpResponse("loged in")
-
+            return redirect('home')
 
     else:
         form = SignUpForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'register-form.html')
 
-    return HttpResponse('signup page')
 def loginpage(request):
-    return HttpResponse('login page')
+    if request.POST:
+        form = LoginForm(request.POST)
+        if form.is_valid:
+            username = form.cleaned_data.get('user_name')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponse('login page')
+    else:
+        form = LoginForm()
+    return render(request, 'signup.html', {'form': form})
